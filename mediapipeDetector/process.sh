c3ea6c29826e2a14ddac3a7ea54542c3b553b6b4
lahiruDilhara <galahirudilhara@gmail.com>
2026-08-29 01:39:50 +0530

    updated pipeline

--- mediapipeDetector/process.sh
+++ mediapipeDetector/process.sh
@@ -1,3 +1,9 @@
+# Ensure all data processing directories exist
+mkdir -p dataprocessing/1_rawCSVFiles
+mkdir -p dataprocessing/2_normalized_coordinates
+mkdir -p dataprocessing/3_euroFilter_coordinates
+mkdir -p dataprocessing/4_filtered_coordinates_and_annotations
+
 # Copy CSV files to data processing directory
 cp -f -r ./videos/*.raw_landmarks.* dataprocessing/1_rawCSVFiles/
 cp -f -r ./videos/*.window_annotations.* dataprocessing/1_rawCSVFiles/
@@ -7,3 +13,7 @@ python3 datacreator/normalize_landmarks.py -i ./dataprocessing/1_rawCSVFiles/*.r
 
 # Filter landmarks using 1Euro Filter
 # python3 datacreator/filter_landmarks.py -i ./dataprocessing/1_rawCSVFiles/*.raw_landmarks.* -o ./dataprocessing/2_coordinationFilteredCSVFiles/
+python3 datacreator/filter_landmarks.py -min 1.5 -beta 1.0 -d 1.0 -i ./dataprocessing/2_normalized_coordinates/*.normalize_landmarks.* -o ./dataprocessing/3_euroFilter_coordinates/
+
+cp -f -r ./dataprocessing/1_rawCSVFiles/*.window_annotations.* ./dataprocessing/4_filtered_coordinates_and_annotations/
+cp -f -r ./dataprocessing/3_euroFilter_coordinates/*.filtered_landmarks.* ./dataprocessing/4_filtered_coordinates_and_annotations/
